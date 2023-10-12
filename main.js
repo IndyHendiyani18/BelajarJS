025048da6d65065de37c0426d0f79d7d59e6daac
IndyHendiyani18 <indyhendiyani@upi.edu>
2023-10-12 14:54:10 +0700

    update promnet pertemuan 6

---
+++ main.js
@@ -0,0 +1,81 @@
+// console.log("Hello World")
+// alert("Notifikasi")
+// prompt("notif apa hayo")
+
+// var Promnet = "Coding is easy"
+// console.log(Promnet)
+// var Promnet = "pi boonk"
+// console.log(Promnet)
+
+// let Promnet = "Coding is easy"
+// console.log(Promnet)
+// let Promnet = "pi boonk"
+// console.log(Promnet)
+
+// const Promnet = "Coding is easy"
+// console.log(Promnet)
+// const Promnet = "pi boonk"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Masukan Poin Anda")
+//     if(totalPoin > 100){
+//         document.write("<h1>Congratulation</h1>");
+//     }
+//     else{
+//         document.write("<h1>Thank You yah</h1>");
+//     }
+
+//CHALLENGE NYA YANG TADI!!!
+// // Definisikan variabel x dan y
+// let x = 6;
+// let y = 3;
+
+// if (x < 10 && y > 1) {
+//   console.log("a = true");
+// } else {
+//   console.log("a = false");
+// }
+
+// if (x < 10 && y < 1) {
+//   console.log("b = true");
+// } else {
+//   console.log("b = false");
+// }
+
+// if (x == 5 || y == 5) {
+//   console.log("c = true");
+// } else {
+//   console.log("c = false");
+// }
+
+// if (x == 6 || y == 5) {
+//   console.log("d = true");
+// } else {
+//   console.log("d = false");
+// }
+
+// if (!(x == y)) {
+//   console.log("e = true");
+// } else {
+//   console.log("e = false");
+// }
+
+// let x = 6;
+// let y = 3;
+
+// console.log(x<10 && y > 1)
+// console.log(x<10 && y < 1) 
+// console.log(x==5||y==5) 
+// console.log(x==6||y==5) 
+// !(x==y);
+
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
